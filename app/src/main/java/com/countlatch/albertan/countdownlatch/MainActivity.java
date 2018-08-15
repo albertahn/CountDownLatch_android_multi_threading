40ad56b96978e4af3f7d485956d12f51065503ca
albertpoynt <albert@poynt.co>
2018-08-15 10:13:45 -0700

    countdown latch project initialized

---
+++ app/src/main/java/com/countlatch/albertan/countdownlatch/MainActivity.java
@@ -0,0 +1,22 @@
+package com.countlatch.albertan.countdownlatch;
+
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity{
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //https://www.stacktips.com/tutorials/java/countdownlatch-and-java-concurrency-example
+
+        //https://developer.android.com/reference/java/util/concurrent/CountDownLatch
+
+
+
+
+    }
+}
