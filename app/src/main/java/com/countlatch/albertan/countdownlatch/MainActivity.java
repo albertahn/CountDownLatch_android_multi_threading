fce5f327a65a8081f2354a134ec17dce0dafd15c
albertpoynt <contendera@gmail.com>
2018-08-15 13:30:26 -0700

    command option L to clean up lines

--- app/src/main/java/com/countlatch/albertan/countdownlatch/MainActivity.java
+++ app/src/main/java/com/countlatch/albertan/countdownlatch/MainActivity.java
@@ -2,8 +2,6 @@ package com.countlatch.albertan.countdownlatch;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.media.Image;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -28,20 +26,20 @@ after which all waiting threads are released and any subsequent invocations of a
  If you need a version that resets the count, consider using a CyclicBarrier.
 * */
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
     String imageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Lion_waiting_in_Namibia.jpg/440px-Lion_waiting_in_Namibia.jpg";
 
     public byte[] imageData;
 
     @Override
-    public void onCreate( Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        try{
+        try {
             getImage();
-        }catch (InterruptedException e){
+        } catch (InterruptedException e) {
 
 
         }
@@ -49,10 +47,9 @@ public class MainActivity extends AppCompatActivity{
     }
 
 
+    private void getImage() throws InterruptedException {
 
-    private void getImage()throws InterruptedException{
-
-        int N =10;
+        int N = 10;
 
         CountDownLatch doneSignal = new CountDownLatch(N);
         MyTaskExecutor e = new MyTaskExecutor();
@@ -64,14 +61,12 @@ public class MainActivity extends AppCompatActivity{
         doneSignal.await();           // wait for all to finish
 
 
-
         ImageView latchImage = findViewById(R.id.latch_image);
 
-        Bitmap bmap  = BitmapFactory.decodeByteArray(imageData, 0, imageData.length);
+        Bitmap bmap = BitmapFactory.decodeByteArray(imageData, 0, imageData.length);
 
 
         latchImage.setImageBitmap(bmap);
-
 
 
     }
@@ -86,10 +81,12 @@ public class MainActivity extends AppCompatActivity{
     class WorkerRunnable implements Runnable {
         private final CountDownLatch doneSignal;
         private final int i;
+
         WorkerRunnable(CountDownLatch doneSignal, int i) {
             this.doneSignal = doneSignal;
             this.i = i;
         }
+
         public void run() {
             try {
                 doWork();
@@ -125,8 +122,6 @@ public class MainActivity extends AppCompatActivity{
 
         }
     }
-
-
 
 
 }
